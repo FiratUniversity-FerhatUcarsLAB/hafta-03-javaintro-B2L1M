b1b3dd2b374b647bd029c53616ef0d3c475fb1fa
Ozan Diyar AY <102673123+B2L1M@users.noreply.github.com>
2025-10-23 23:55:42 +0300

    AsciiArtHarf java, png ve class dosyaları yüklendi.

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,14 @@
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+        String oHarfi = """
+                 #######
+               #         #
+              #           #
+              #           #
+               #         #
+                 #######
+                """;
+                
+        System.out.println(oHarfi);
+    }
+}
