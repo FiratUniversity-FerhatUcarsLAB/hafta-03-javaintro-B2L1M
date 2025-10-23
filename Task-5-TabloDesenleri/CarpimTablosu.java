3c8bde83dd62d66e3722983d60338fc2ecc0811e
Ozan Diyar AY <102673123+B2L1M@users.noreply.github.com>
2025-10-24 00:24:05 +0300

    CarpimTablosu java, class ve png dosyaları yüklendi.

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,37 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+
+        System.out.println("2'nin Çarpım Tablosu");
+        System.out.println("--------------------");
+
+        int n1 = 1;
+        System.out.println("2 x " + n1 + "  = " + (2 * n1));
+
+        int n2 = 2;
+        System.out.println("2 x " + n2 + "  = " + (2 * n2));
+
+        int n3 = 3;
+        System.out.println("2 x " + n3 + "  = " + (2 * n3));
+
+        int n4 = 4;
+        System.out.println("2 x " + n4 + "  = " + (2 * n4));
+
+        int n5 = 5;
+        System.out.println("2 x " + n5 + "  = " + (2 * n5));
+
+        int n6 = 6;
+        System.out.println("2 x " + n6 + "  = " + (2 * n6));
+
+        int n7 = 7;
+        System.out.println("2 x " + n7 + "  = " + (2 * n7));
+
+        int n8 = 8;
+        System.out.println("2 x " + n8 + "  = " + (2 * n8));
+
+        int n9 = 9;
+        System.out.println("2 x " + n9 + "  = " + (2 * n9));
+
+        int n10 = 10;
+        System.out.println("2 x " + n10 + " = " + (2 * n10));
+    }
+}
