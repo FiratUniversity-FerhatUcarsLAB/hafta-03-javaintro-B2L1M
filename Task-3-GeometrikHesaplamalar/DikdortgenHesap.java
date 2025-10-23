84763f85dd23d4d898c9ab155f6552949f0812c7
Ozan Diyar AY <102673123+B2L1M@users.noreply.github.com>
2025-10-23 23:45:26 +0300

    DikdortgenHesap java, class ve png dosyaları yüklendi.

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,12 @@
+public class DikdortgenHesap {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+        double cevre = 2 * (en + boy);
+        double alan = en * boy;
+        System.out.println("Dikdortgenin Eni: " + en);
+        System.out.println("Dikdortgenin Boyu: " + boy);
+        System.out.println("Dikdortgenin Cevresi: " + cevre);
+        System.out.println("Dikdortgenin Alani: " + alan);
+    }
+}
