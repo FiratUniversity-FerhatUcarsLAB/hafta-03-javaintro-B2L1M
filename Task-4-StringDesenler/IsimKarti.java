f91fd7201350ad460659e9893d92c2142b4ac9e5
Ozan Diyar AY <102673123+B2L1M@users.noreply.github.com>
2025-10-24 00:00:25 +0300

    IsimKarti java, class ve png dosyaları yüklendi.

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Ozan Diyar";
+        String soyad = "AY";
+        String no = "250541062";
+        String bolum = "Yazilim Muh.";
+
+        System.out.println("=======================");
+        System.out.println("   OGRENCI BILGILERI   ");
+        System.out.println("=======================");
+        System.out.println("Ad     : " + ad);
+        System.out.println("Soyad  : " + soyad);
+        System.out.println("No     : " + no);
+        System.out.println("Bolum  : " + bolum);
+        System.out.println("=======================");
+    }
+}
