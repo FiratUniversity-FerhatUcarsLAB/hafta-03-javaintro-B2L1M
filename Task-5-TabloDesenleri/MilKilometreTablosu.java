82fb47a3c96e1e639fcad06adbbe3580f5538332
Ozan Diyar AY <102673123+B2L1M@users.noreply.github.com>
2025-10-24 00:21:13 +0300

    MilKilometreTablosu java, class ve png dosyaları yüklendi.

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,24 @@
+public class MilKilometreTablosu {
+    public static void main(String[] args) {
+
+        double km = 1.609;
+
+        System.out.println("Mil" + "   " + "Kilometre");
+        System.out.println("---------------");
+
+        int mil1 = 1;
+        System.out.println(mil1 + "     " + (mil1 * km));
+
+        int mil2 = 5;
+        System.out.println(mil2 + "     " + (mil2 * km));
+
+        int mil3 = 10;
+        System.out.println(mil3 + "    " + (mil3 * km));
+
+        int mil4 = 20;
+        System.out.println(mil4 + "    " + (mil4 * km));
+
+        int mil5 = 50;
+        System.out.println(mil5 + "    " + (mil5 * km));
+    }
+}
